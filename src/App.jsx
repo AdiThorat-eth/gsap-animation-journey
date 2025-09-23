08c2307285bc135eb8eb83cd050decf6824e9581
Aditya Thorat <a.workspace.00@gmail.com>
2025-09-23 08:27:08 +0530

    Added css

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ gsap.registerPlugin(ScrollTrigger, SplitText);
 const App = () => {
   return (
     <div>
-      <h1 className="text-5xl text-red-400 flex-center"> Yohohoho </h1>
+      <h1 className="flex-center h-[100vh]"> Yohohoho </h1>
     </div>
   );
 };
