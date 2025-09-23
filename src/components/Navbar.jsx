a7af0b956e45cad932ebf44884c0e053f05c98ac
Aditya Thorat <a.workspace.00@gmail.com>
2025-09-23 17:36:07 +0530

    Added navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { navLinks } from "../../constants/index.js";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+const Navbar = () => {
+
+  useGSAP(() => {
+    const navTween = gsap.timeline({
+      scrollTrigger:{
+        trigger: "nav",
+        start: "bottom top",
+
+      }
+    })
+
+    navTween.fromTo("nav", {
+      backgroundColor: "transparent",
+    },
+    {
+      backgroundColor: "#00000050",
+      backgroundFilter: "blur(10px)",
+      duration: 1,
+      ease: "power1.inOut",
+    }
+  )
+  })
+
+  return (
+    <nav>
+      <div>
+        <a href="#hero" className="flex items-center gasp-2">
+          <img src="/images/logo.png" alt="logo" />
+          <p>Vinsmoke</p>
+        </a>
+        <ul>
+          {navLinks.map(
+            (
+              link // if you use "{}" instead of "()" you need to use "return"
+            ) => (
+              <li key={link.id}>
+                <a href={`#${link.id}`}>{link.title}</a>
+              </li>
+            )
+          )}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
