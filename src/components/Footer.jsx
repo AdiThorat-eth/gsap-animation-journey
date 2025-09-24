901253aeb296dae64397829a128ef952b56da5bf
Aditya Thorat <a.workspace.00@gmail.com>
2025-09-25 00:41:12 +0530

    Added footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,105 @@
+import React from "react";
+import { openingHours, socials } from "../../constants";
+import { useGSAP } from "@gsap/react";
+import { SplitText } from "gsap/all";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+const Footer = () => {
+  useGSAP(() => {
+    const titleSplit = SplitText.create("#contact h2", { type: "words" });
+    const timeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#contact",
+        start: "top center",
+      },
+      ease: "power1.inOut",
+    });
+    timeline
+      .from(titleSplit.words, {
+        opacity: 0,
+        yPercent: 100,
+        stagger: 0.02,
+      })
+      .from("#contact h3, #contact p", {
+        opacity: 0,
+        yPercent: 100,
+        stagger: 0.02,
+      })
+      .to("#f-right-leaf", {
+        y: "50",
+        duration: 1,
+        scale: 1.2,
+        ease: "power1.inOut",
+      })
+      .to(
+        "#f-left-leaf",
+        {
+          y: "-20",
+          duration: 1,
+          scale: 1.2,
+          ease: "power1.inOut",
+        },
+        "<"
+      );
+  });
+
+  return (
+    <footer id="contact">
+      <img
+        src="/images/footer-right-leaf.png"
+        alt="leaf-right"
+        id="f-right-leaf"
+      />
+
+      <img
+        src="/images/footer-left-leaf.png"
+        alt="leaf-left"
+        id="f-left-leaf"
+      />
+
+      <div className="content">
+        <h2>You Want Us</h2>
+        <div>
+          <h3>Reach to us</h3>
+          <p>Shop GP 8A, Raghuleela Mega Mall, Kandivali West, Mumbai</p>
+        </div>
+
+        <div>
+          <h3>Call us</h3>
+          <p>+91-1234567890</p>
+          <p>mr.prince@onepiece.com</p>
+        </div>
+
+        <div>
+          <h3>At Your Service</h3>
+          {openingHours.map((time) => (
+            <p key={time.day}>
+              {time.day} : {time.time}
+            </p>
+          ))}
+        </div>
+
+        <div>
+          <h3>Socials</h3>
+          <div className="flex-center gap-5">
+            {socials.map((social) => (
+              <a
+                key={social.name}
+                href={social.url}
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label={social.name}
+              >
+                <img src={social.icon} alt="" />
+              </a>
+            ))}
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
